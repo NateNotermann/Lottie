d4dcdddaf2113e621cfa18637aa1b692691d66e3
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2023-08-12 02:18:46 -0500

    git lottie to work with Live Server

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,7 +7,8 @@ var animation = bodymovin.loadAnimation({
     // path: 'data.json'
     // path: 'LottieTest1.json'
     // path: 'https://lottie.host/5f723f19-9bfa-4722-bddc-ae67ea7ebe51/dsxsVXdEd9.json'
-    path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+    // path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+    path: '/UseFrame1.2.json'
     // path: 'https://lottie.host/83ad5a3f-aab3-4902-9e52-be603c3c0c04/Sm9uQvldsh.json'
   });
 
