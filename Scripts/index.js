3ebf6b36ef1e7fb0ebb89bec051f337706e395c1
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2023-08-11 22:50:00 -0500

    internet demo works only

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,6 +3,10 @@ var animation = bodymovin.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'data.json'
-})
+    name: 'LottieTest1',
+    // path: 'data.json'
+    // path: 'LottieTest1.json'
+    // path: 'https://lottie.host/79735523-f58a-4208-bbe9-23b9a90de163/hpzsj7WsaM.json'
+    path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+  })
 
